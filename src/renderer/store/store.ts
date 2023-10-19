0c316d29ad60fca0c555c0cb11866c43907dc7ca
Sushant S Samuel <iam@robosushie.com>
2023-10-19 13:38:11 +0530

    Refactored zustand store

--- src/renderer/store/store.ts
+++ src/renderer/store/store.ts
@@ -1,5 +1,17 @@
 import { create } from "zustand";
 
+type TransformControls = {
+  mode: "translate" | "scale" | "rotate";
+  space: "world" | "local";
+};
+
+type Insights = {
+  "vertex-count": number;
+  "triangle-count": number;
+  "mesh-count": number;
+  "texture-count": number;
+};
+
 type GlobalStore = {
   assetLoadingInProgress: boolean;
   pendingFileList: any;
@@ -8,80 +20,65 @@ type GlobalStore = {
   selectionHighlight: boolean;
   hoverHighlight: boolean;
   selectionMode: "object" | "group" | "mesh";
-  transformControls: {
-    mode: "translate" | "scale" | "rotate";
-    space: "world" | "local";
-  };
+  transformControls: TransformControls;
   gridVisibility: boolean;
   axisVisibility: boolean;
   assetList: any;
   enableSelectedObjectInsights: boolean;
-  insights: {
-    "vertex-count": number;
-    "triangle-count": number;
-    "mesh-count": number;
-    "texture-count": number;
-  };
-  setAssetLoadingInProgress: (assetLoadingInProgress: boolean) => void;
-  setPendingFileList: (pendingFileList: any) => void;
-  setSelectedObject: (selectedObject: any) => void;
-  setHoverObject: (hoverObject: any) => void;
-  setSelectionHighlight: (selectionHighlight: boolean) => void;
-  setHoverHighlight: (hoverHighlight: boolean) => void;
-  setSelectionMode: (selectionMode: "object" | "group" | "mesh") => void;
-  setTransformControls: (transformControls: {
-    mode: "translate" | "scale" | "rotate";
-    space: "world" | "local";
-  }) => void;
-  setGridVisibility: (gridVisibilty: boolean) => void;
-  setAxisVisibility: (axesVisibility: boolean) => void;
-  setAssetList: (assetList: any) => void;
-  setInsights: (insights: {
-    "vertex-count": number;
-    "triangle-count": number;
-    "mesh-count": number;
-    "texture-count": number;
-  }) => void;
-  setEnableSelectedObjectInsights: (
-    enableSelectedObjectInsights: boolean
-  ) => void;
+  insights: Insights;
+
+  // Setter methods will be created dynamically
 };
 
-export const useGlobalStore = create<GlobalStore>((set) => ({
-  assetLoadingInProgress: false,
-  pendingFileList: null,
-  selectedObject: null,
-  hoverObject: null,
-  selectionHighlight: false,
-  hoverHighlight: true,
-  selectionMode: "object",
-  gridVisibility: true,
-  transformControls: { mode: "translate", space: "world" },
-  axisVisibility: true,
-  assetList: [],
-  enableSelectedObjectInsights: false,
-  insights: {
-    "vertex-count": 0,
-    "triangle-count": 0,
-    "mesh-count": 0,
-    "texture-count": 0,
-  },
-  setAssetLoadingInProgress: (assetLoadingInProgress) =>
-    set({ assetLoadingInProgress }),
-  setPendingFileList: (pendingFileList) => set({ pendingFileList }),
-  setSelectedObject: (selectedObject) => set({ selectedObject }),
-  setHoverObject: (hoverObject) => set({ hoverObject }),
-  setSelectionHighlight: (selectionHighlight) => set({ selectionHighlight }),
-  setHoverHighlight: (hoverHighlight) => set({ hoverHighlight }),
-  setSelectionMode: (selectionMode) => set({ selectionMode }),
-  setTransformControls: (transformControls: {
-    mode: "translate" | "scale" | "rotate";
-    space: "world" | "local";
-  }) => set({ transformControls }),
-  setGridVisibility: (gridVisibility) => set({ gridVisibility }),
-  setAxisVisibility: (axisVisibility) => set({ axisVisibility }),
-  setAssetList: (assetList) => set({ assetList }),
-  setInsights: (insights) => set({ insights }),
-  setEnableSelectedObjectInsights: (enableSelectedObjectInsights) =>
-    set({ enableSelectedObjectInsights }),
-}));
+const createSetter = (key: keyof GlobalStore) => (value: any) => ({
+  [key]: value,
+});
+
+export const useGlobalStore = create<GlobalStore>((set) => {
+  const setters = Object.keys({
+    assetLoadingInProgress: false,
+    pendingFileList: null,
+    selectedObject: null,
+    hoverObject: null,
+    selectionHighlight: false,
+    hoverHighlight: true,
+    selectionMode: "object",
+    gridVisibility: true,
+    transformControls: { mode: "translate", space: "world" },
+    axisVisibility: true,
+    assetList: [],
+    enableSelectedObjectInsights: false,
+    insights: {
+      "vertex-count": 0,
+      "triangle-count": 0,
+      "mesh-count": 0,
+      "texture-count": 0,
+    },
+  }).reduce((acc: any, key) => {
+    acc[`set${key.charAt(0).toUpperCase() + key.slice(1)}`] = (value: any) =>
+      set(createSetter(key as keyof GlobalStore)(value));
+    return acc;
+  }, {} as GlobalStore);
+
+  return {
+    ...setters,
+    assetLoadingInProgress: false,
+    pendingFileList: null,
+    selectedObject: null,
+    hoverObject: null,
+    selectionHighlight: false,
+    hoverHighlight: true,
+    selectionMode: "object",
+    gridVisibility: true,
+    transformControls: { mode: "translate", space: "world" },
+    axisVisibility: true,
+    assetList: [],
+    enableSelectedObjectInsights: false,
+    insights: {
+      "vertex-count": 0,
+      "triangle-count": 0,
+      "mesh-count": 0,
+      "texture-count": 0,
+    },
+  };
+});
